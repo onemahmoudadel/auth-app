2bdd2949e15b3490a0ceb248dc73c896db0418df
Mahmoud Adel <onemahmoudadel@gamil.com>
2024-06-07 00:12:08 +0300

    add react router and shadncn blocks

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import { HomePage } from './components/Home';
+import { Settings } from './components/Settings';
+import { SignUp } from './components/SignUp';
+import { Login } from './components/Login';
+import { MainLayout } from './components/MainLayout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+    {
+      index: true,
+      element: <HomePage />,
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+    {
+      path: "signup",
+      element: <SignUp />,
+    },
+    {
+      path: "settings",
+      element: <Settings />,
+    },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
