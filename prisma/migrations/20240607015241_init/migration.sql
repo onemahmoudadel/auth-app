03d76cf45a1d1926a61f907c16a4e2ba57d90f9f
Mahmoud Adel <onemahmoudadel@gamil.com>
2024-06-07 06:50:38 +0300

    tanstack/react-query

---
+++ prisma/migrations/20240607015241_init/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `firstName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `password_hash` on the `User` table. All the data in the column will be lost.
+  - Added the required column `firstname` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `hashpassword` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastname` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "hashpassword" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("email", "id") SELECT "email", "id" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check("User");
+PRAGMA foreign_keys=ON;
---
+++ prisma/migrations/20240607034924_init/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `firstname` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `hashpassword` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `lastname` on the `User` table. All the data in the column will be lost.
+  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password_hash` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("email", "id") SELECT "email", "id" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check("User");
+PRAGMA foreign_keys=ON;
